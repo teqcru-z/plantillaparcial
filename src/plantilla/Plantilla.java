33e91256b3c1dd0fe7cae8208a85ab2b4fbd96b9
tequi <tequi@DESKTOP-17N706G>
2026-04-28 12:07:32 -0600

    Inicio Plantilla

---
+++ src/plantilla/Plantilla.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package plantilla;
+
+/**
+ *
+ * @author tequi
+ */
+public class Plantilla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
